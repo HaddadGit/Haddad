aa2f03ae50e16f83c2d27dc4aa364bb89e8a1347
Mohammad Haddad <haddad.94m@gmail.com>
2020-07-04 12:44:50 +0430

    Updates

---
+++ precache-manifest.1ba032b421ecb1324ece09a55aff7ff2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29adca4cb4eb6bd40b770e2461faf311",
+    "url": "/haddad/index.html"
+  },
+  {
+    "revision": "17c3612d247f71e21df4",
+    "url": "/haddad/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "5ab2df51ac8cbddebe77",
+    "url": "/haddad/static/js/2.aa633ab9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/haddad/static/js/2.aa633ab9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "17c3612d247f71e21df4",
+    "url": "/haddad/static/js/main.b07d01ee.chunk.js"
+  },
+  {
+    "revision": "38be8213f43811e86d8a",
+    "url": "/haddad/static/js/runtime-main.95143c01.js"
+  }
+]);
